6090c17669918ef30d29afa2f6561e5924cbf327
Aghalya Narayanan <aghalya2@illinois.edu>
2023-05-22 12:12:29 -0700

    added states & ability to add expenses w/ date tracker

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,29 @@
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
+import React, {useState} from 'react';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
     // const expenseDate = new Date(2021, 2, 28);
     // const expenseTitle = 'Car Insurance';
     // const expenseAmt = 294.67;
+    const [title, setTitle] = useState(props.title);
 
+    const clickHandler = () => {
+        // title = 'Updated';
+        setTitle('Updated');
+        console.log('Clicked!');
+    };
 
+    // let title = props.title
     return (
     <Card className = "expense-item">
         <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
     </Card>
     );
 }
